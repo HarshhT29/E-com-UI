c64394fc3a2544e0ed11a700218bb5680df4582f
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2023-09-14 22:21:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,27 +5,36 @@ const close = document.getElementById('close');
 if(bar) {
     bar.addEventListener('click', ()=>{
         nav.classList.add('active');
+        nav.classList.remove('hidden');
     })
 }
 if(close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
+        nav.classList.add('hidden');
     })
 }
 
 // <---js of single product----------->
 var mainImg = document.getElementById("MainImg");
 var smallImg = document.getElementsByClassName("sm-img");
-
-smallImg[0].onclick = function(){
-    mainImg.src = smallImg[0].src;
+if (smallImg[0]) {
+    smallImg[0].onclick = function(){
+        mainImg.src = smallImg[0].src;
+    }
 }
-smallImg[1].onclick = function(){
-    mainImg.src = smallImg[1].src;
+if (smallImg[1]) {
+    smallImg[1].onclick = function(){
+        mainImg.src = smallImg[1].src;
+    }
 }
-smallImg[2].onclick = function(){
-    mainImg.src = smallImg[2].src;
+if (smallImg[2]) {
+    smallImg[2].onclick = function(){
+        mainImg.src = smallImg[2].src;
+    }
 }
-smallImg[3].onclick = function(){
-    mainImg.src = smallImg[3].src;
+if (smallImg[3]) {
+    smallImg[3].onclick = function(){
+        mainImg.src = smallImg[3].src;
+    }
 }
